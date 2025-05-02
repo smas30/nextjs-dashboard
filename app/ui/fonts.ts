8c7a8b116ae8e562b03e1fb1f319eae0ec476ee8
Sarai Silva <saraisilva211@gmail.com>
2025-05-02 19:24:54 -0400

    Changing fonts, images and colors.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+//Fuentes que se utilizarán en toda la aplicación.
+import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
+import { Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
